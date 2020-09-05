b77861e74b48a62fad49700a4b72bcd4f3535035
jainal09 <jainal09gosalia@gmail.com>
2020-09-05 23:20:46 +0530

    kafka authentication added

--- init.py
+++ init.py
@@ -8,7 +8,7 @@ import json
 # Redis initialize
 redis_obj = redis.StrictRedis(
     host=globals.REDIS_HOSTNAME,
-    port= globals.KAFKA_PORT,
+    port=globals.KAFKA_PORT,
     password=globals.REDIS_PASSWORD,
     ssl=True
 )
@@ -21,9 +21,17 @@ consumer_obj = KafkaConsumer(
     enable_auto_commit=True,
     group_id="my-group",
     value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+    security_protocol="SASL_PLAINTEXT",
+    sasl_mechanism='PLAIN',
+    sasl_plain_username=globals.KAFKA_USERNAME,
+    sasl_plain_password=globals.KAFKA_PASSWORD
 )
 
 producer_obj = KafkaProducer(
     bootstrap_servers=[globals.KAFKA_HOSTNAME + ':' + globals.KAFKA_PORT],
     value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+    security_protocol="SASL_PLAINTEXT",
+    sasl_mechanism='PLAIN',
+    sasl_plain_username=globals.KAFKA_USERNAME,
+    sasl_plain_password=globals.KAFKA_PASSWORD
 )
